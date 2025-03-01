cc8ba2c005cac5d9ad76b968386bb030238a421a
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2025-03-01 09:05:07 -0700

    minor changes

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -5,45 +5,51 @@ import SplashScreen from "../components/SplashScreen";
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import AppProvider from "../components/providers";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { SessionProvider } from "next-auth/react";
 
 import { config } from '../wagmi';
 import { CampaignProvider } from '../context/CampaignContext';
+import Header from '../components/Header';
 
 const client = new QueryClient();
 
-async function MyApp({ Component, pageProps }: AppProps) {
-
+function MyApp({ Component, pageProps }: AppProps & { pageProps: { session?: any } }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 3000);
   }, []);
 
-  const session = await getServerSession(authOptions);
-
   return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={client}>
-        <RainbowKitProvider>
-          <Head>
-            <link rel="icon" href="/favicon.ico" />
-            <title>FairLaunch</title>
-          </Head>
-          <CampaignProvider>
-            <AppProvider session={session}>
-            <div className="min-h-screen bg-[#0D0D0D] text-white font-poppins">
-              {loading ? <SplashScreen onFinish={() => setLoading(false)} /> : <Component {...pageProps} />}
-            </div>
-            </AppProvider>
-          </CampaignProvider>
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <SessionProvider session={pageProps.session}>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={client}>
+          <RainbowKitProvider>
+            <Head>
+              <link rel="icon" href="/favicon.ico" />
+              <title>FairLaunch</title>
+            </Head>
+            <CampaignProvider>
+              {/* Ensure session is passed to AppProvider */}
+              <AppProvider session={pageProps.session}>
+                <div className="min-h-screen bg-[#0D0D0D] text-white font-poppins">
+                  {loading ? <SplashScreen onFinish={() => setLoading(false)} /> : 
+                    <div>
+                      <Header />
+                      <Component {...pageProps} />
+                    </div>
+                    
+                  }
+                </div>
+              </AppProvider>
+            </CampaignProvider>
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </SessionProvider>
   );
 }
 
