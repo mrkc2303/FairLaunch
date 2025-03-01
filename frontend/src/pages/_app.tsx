5867d4cc01453d97d764d79eedbbf5a61284ed63
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2025-03-01 08:08:14 -0700

    Octo COde

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,7 +4,9 @@ import "../styles/globals.css";
 import SplashScreen from "../components/SplashScreen";
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-
+import AppProvider from "../components/providers";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
@@ -14,13 +16,15 @@ import { CampaignProvider } from '../context/CampaignContext';
 
 const client = new QueryClient();
 
-function MyApp({ Component, pageProps }: AppProps) {
+async function MyApp({ Component, pageProps }: AppProps) {
 
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 3000);
   }, []);
+
+  const session = await getServerSession(authOptions);
 
   return (
     <WagmiProvider config={config}>
@@ -31,9 +35,11 @@ function MyApp({ Component, pageProps }: AppProps) {
             <title>FairLaunch</title>
           </Head>
           <CampaignProvider>
+            <AppProvider session={session}>
             <div className="min-h-screen bg-[#0D0D0D] text-white font-poppins">
               {loading ? <SplashScreen onFinish={() => setLoading(false)} /> : <Component {...pageProps} />}
             </div>
+            </AppProvider>
           </CampaignProvider>
         </RainbowKitProvider>
       </QueryClientProvider>
